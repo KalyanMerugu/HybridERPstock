e788bd9e46f95894f9ba66a1e35d64d10b643973
Sai Prasad <Sai Prasad@LAPTOP-5R43AL2Q>
2024-10-23 14:46:41 +0530

    Writen Methods and Excel oct 16

--- src/test/java/commonFunctions/FunctionLibrary.java
+++ src/test/java/commonFunctions/FunctionLibrary.java
@@ -1,5 +1,63 @@
 package commonFunctions;
 
-public class FunctionLibrary {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+
+public class FunctionLibrary {
+public static WebDriver driver;
+public static Properties conpro;
+//method for launching browser
+public static WebDriver startBrowser() throws Throwable
+{
+	conpro = new Properties();
+	conpro.load(new FileInputStream("PropertyFiles\\Environement.properties"));
+	if(conpro.getProperty("Browser").equalsIgnoreCase("Chrome"))
+	{
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	else if(conpro.getProperty("Browser").equalsIgnoreCase("firefox"))
+	{
+		driver = new FirefoxDriver();
+	}
+	else
+	{
+		Reporter.log("Browser value is not matching",true);
+	}
+	return driver;
+}
+//method for launching url
+public static void openUrl()
+{
+	driver.get(conpro.getProperty("Url"));
+}
+//method for wait any element 
+public static void waitForElement(String LocatorType, String locLocatorValue, String TestData)
+{
+	WebDriverWait mywait = new WebDriverWait(driver, Duration.ofSeconds(Integer.parseInt(TestData)));
+		if(LocatorType.equalsIgnoreCase("xpath"))	
+		{
+			//wait untill elememt is visible
+			mywait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locLocatorValue)));
+		}
+		if(LocatorType.equalsIgnoreCase("name"))
+		{
+			mywait.until(ExpectedConditions.visibilityOfElementLocated(By.name(locLocatorValue)));
+		}
+		if(LocatorType.equalsIgnoreCase("id"))
+		{
+			mywait.until(ExpectedConditions.visibilityOfElementLocated(By.id(locLocatorValue)));
+		}
+}
 }
